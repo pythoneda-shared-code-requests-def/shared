7235c5305eafbb710b1f4bbf9b877350825018be
rydnr <github@acm-sl.org>
2023-12-17 11:34:53 +0100

    Removed jq hack

--- flake.nix
+++ flake.nix
@@ -103,7 +103,7 @@
 
             format = "pyproject";
 
-            nativeBuildInputs = with python.pkgs; [ pip pkgs.jq poetry-core ];
+            nativeBuildInputs = with python.pkgs; [ pip poetry-core ];
             propagatedBuildInputs = with python.pkgs; [
               pythoneda-shared-nix-flake-shared
               pythoneda-shared-pythoneda-domain
@@ -128,7 +128,6 @@
               popd
               mkdir $out/dist
               cp dist/${wheelName} $out/dist
-              jq ".url = \"$out/dist/${wheelName}\"" $out/lib/python${pythonMajorMinorVersion}/site-packages/${pnameWithUnderscores}-${version}.dist-info/direct_url.json > temp.json && mv temp.json $out/lib/python${pythonMajorMinorVersion}/site-packages/${pnameWithUnderscores}-${version}.dist-info/direct_url.json
             '';
 
             meta = with pkgs.lib; {
