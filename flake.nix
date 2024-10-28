de09f1219597b21ba64b7e75157bfc4b43eef4c9
rydnr <github@acm-sl.org>
2024-10-28 18:58:06 +0100

    Updated nixpkgs

--- flake.nix
+++ flake.nix
@@ -20,7 +20,7 @@
   description = "Shared kernel for pythoneda-shared-code-requests/shared";
   inputs = rec {
     flake-utils.url = "github:numtide/flake-utils/v1.0.0";
-    nixos.url = "github:NixOS/nixpkgs/23.11";
+    nixos.url = "github:NixOS/nixpkgs/24.05";
     pythoneda-shared-nix-flake-shared = {
       inputs.flake-utils.follows = "flake-utils";
       inputs.nixos.follows = "nixos";
